e43febd32d75b8325081140a7ee3dcb4a0db1997
Takahiro Hara <takahiro.hb@gmail.com>
2023-02-05 22:37:19 +0900

    ユーザー登録処理の一部を追加
    - リクエストの発酵のみ

---
+++ src/features/register/apis/register_api.ts
@@ -0,0 +1,47 @@
+import { requestApi } from '@/utils/api';
+
+interface IRegisterRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+interface IUser {
+  id: number;
+  name: string;
+  email: string;
+  activated: boolean;
+}
+
+interface IRegisterError {
+  msg: string | null;
+  name: string | null;
+  email: string | null;
+  password: string | null;
+}
+
+interface IRegisterResponse {
+  user: IUser;
+  error: IRegisterError;
+}
+
+export const postUser = async (name: string, email: string, password: string) => {
+  try {
+    const { user, error } = await requestApi<IRegisterRequest, IRegisterResponse>(
+      '/v1/users',
+      'POST',
+      {
+        name: name,
+        email: email,
+        password: password,
+      },
+    );
+
+    return { user, error };
+  } catch (error) {
+    if (error instanceof Error) {
+      return { user: null, error: { msg: error.message } as IRegisterError };
+    }
+    return { user: null, error: { msg: 'エラーが発生しました' } as IRegisterError };
+  }
+};
--- src/features/register/register_container.tsx
+++ src/features/register/register_container.tsx
@@ -1,7 +1,19 @@
 import styles from '@/features/register/register_container.module.scss';
 import Head from 'next/head';
+import { useState } from 'react';
+import { postUser } from './apis/register_api';
 
 export const RegisterContainer = () => {
+  const [name, setName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const register = async () => {
+    const { user, error } = await postUser(name, email, password);
+    console.log(user);
+    console.log(error);
+  };
+
   return (
     <>
       <Head>
@@ -15,21 +27,44 @@ export const RegisterContainer = () => {
             <label htmlFor='name' className={styles.label}>
               ユーザー名
             </label>
-            <input type='text' name='name' id='name' className={styles.input} />
+            <input
+              type='text'
+              name='name'
+              id='name'
+              className={styles.input}
+              value={name}
+              onChange={e => setName(e.target.value)}
+            />
           </div>
           <div className={styles.inputWrapper}>
             <label htmlFor='email' className={styles.label}>
               メールアドレス
             </label>
-            <input type='email' name='email' id='email' className={styles.input} />
+            <input
+              type='email'
+              name='email'
+              id='email'
+              className={styles.input}
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
           </div>
           <div className={styles.inputWrapper}>
             <label htmlFor='password' className={styles.label}>
               パスワード
             </label>
-            <input type='password' name='password' id='password' className={styles.input} />
+            <input
+              type='password'
+              name='password'
+              id='password'
+              className={styles.input}
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
           </div>
-          <button className={styles.button}>登録</button>
+          <button className={styles.button} onClick={register}>
+            登録
+          </button>
         </div>
       </main>
     </>
