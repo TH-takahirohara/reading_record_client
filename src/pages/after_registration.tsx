b5d4d0dcfdce21cfd9d3556202128fa44af0b81a
Takahiro Hara <takahiro.hb@gmail.com>
2023-02-06 00:56:09 +0900

    登録後に遷移する画面の追加

---
+++ src/pages/after_registration.tsx
@@ -0,0 +1,6 @@
+import { AfterRegistrationContainer } from '@/features/after_registration/after_registration_container';
+import type { NextPage } from 'next';
+
+const AfterRegistrationPage: NextPage = () => <AfterRegistrationContainer />;
+
+export default AfterRegistrationPage;
