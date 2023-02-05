31f0a4927d3c4ff72512455233741f898c7526fe
Takahiro Hara <takahiro.hb@gmail.com>
2023-02-05 22:35:13 +0900

    APIへのリクエスト処理に用いる関数の追加

---
+++ src/utils/api.ts
@@ -0,0 +1,18 @@
+import { Constants } from '@/constants/constants';
+
+export const fetcher = async <T = any>(url: string, option?: RequestInit): Promise<T> => {
+  const response = await fetch(url, option);
+  return response.json();
+};
+
+type RequestMethod = 'GET' | 'POST' | 'PATCH' | 'DELETE';
+
+export const requestApi = <T, U>(path: string, method: RequestMethod, body?: T) => {
+  return fetcher<U>(`${Constants.shared().apiOrigin}${path}`, {
+    method,
+    body: JSON.stringify(body),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+};
