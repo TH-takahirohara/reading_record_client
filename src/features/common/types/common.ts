63a277242443325e00e3eb4b9a12ca5413729787
Takahiro Hara <takahiro.hb@gmail.com>
2023-02-08 23:35:42 +0900

    インタフェースに関する共通ファイルの作成、および読書記録関連のインタフェースを同ファイルに追加

---
+++ src/features/common/types/common.ts
@@ -0,0 +1,15 @@
+export interface IReading {
+  id: number;
+  bookName: string;
+  bookAuthor: string;
+  totalPageCount: number;
+  currentPage: number;
+  memo: string;
+  dailyProgresses: IDailyProgress | null;
+}
+
+export interface IDailyProgress {
+  id: number;
+  readDate: string;
+  readPage: number;
+}
